322d8dddc366f046f10128a0ce7eb48909d90088
sumit <sumitqwer1234@gmail.com>
2024-11-02 00:29:56 +0530

    configure multer

--- src/utils/ApiErrors.js
+++ src/utils/ApiErrors.js
@@ -3,7 +3,7 @@ class ApiErrors extends Error {
         stateCode,
         message = "Something went wrong",
         errors = [],
-        statck = ""
+        stack = ""
     ){
         super(message)
         this.stateCode = stateCode
@@ -12,8 +12,8 @@ class ApiErrors extends Error {
         this.success = false
         this.errors = errors
 
-        if(statck){
-            this.stack = statck
+        if(stack){
+            this.stack = stack
         } else {
             Error.captureStackTrace(this, this.constructor)
         }
